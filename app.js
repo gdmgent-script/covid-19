8b9095e78200e7a8e9dab31323e7a5020993d2cb
Frederick Roegiers <frederick@rogerthat.be>
2020-03-23 11:57:27 +0100

    wip

---
+++ app.js
@@ -0,0 +1,24 @@
+// declare media element (video)
+const media = document.querySelector('video');
+
+// declare every button
+
+
+// declare an object with key and value
+// const timeline = {
+//     avoidContact: 14,
+//     ...
+// }
+
+// event listeners on each button (click event)
+    // play video
+    // disable all buttons while playing
+
+// event listener on play, to pause automatically after 3 seconds
+media.addEventListener('play', (e) => {
+    
+    // timeout after 3 seconds
+        // media.pause();
+        // enable all buttons
+
+});
