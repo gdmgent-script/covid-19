34594539e6cb837906e0b2780c2dbd3f68d11c65
Frederick Roegiers <frederick@rogerthat.be>
2020-03-23 12:00:26 +0100

    wip

--- app.js
+++ app.js
@@ -2,23 +2,71 @@
 const media = document.querySelector('video');
 
 // declare every button
+const buttons = document.querySelectorAll('button'); // alle knoppen
+const btnWash = document.querySelector('#wasjehanden');
+const btnFace = document.querySelector('#gezichtnietaanraken');
+const btnContact = document.querySelector('#vermijdcontact');
+const btnHome = document.querySelector('#blijfinuwkot');
+const btnWalk = document.querySelector('#loopweg');
 
-
-// declare an object with key and value
-// const timeline = {
-//     avoidContact: 14,
-//     ...
-// }
+// declare an object (timeline) with key and value
+const timeline = {
+    washHands: 0,
+    dontTouchFace: 8,
+    avoidContact: 14,
+    stayHome: 21,
+    walkAway: 24    
+};
 
 // event listeners on each button (click event)
+btnWash.addEventListener('click', (e) => {
+    media.currentTime = timeline.washHands;
+    playMedia();
+});
+
+btnFace.addEventListener('click', (e) => {
+    media.currentTime = timeline.dontTouchFace;
+    playMedia();
+});
+
+btnContact.addEventListener('click', (e) => {
+    media.currentTime = timeline.avoidContact;
+    playMedia();
+});
+
+btnHome.addEventListener('click', (e) => {
+    media.currentTime = timeline.stayHome;
+    playMedia();
+});
+
+
+btnWalk.addEventListener('click', (e) => {
+    media.currentTime = timeline.walkAway;
+    playMedia();
+});
+
+// play the video and disable every button
+function playMedia() {
     // play video
-    // disable all buttons while playing
+    media.play();
+
+    // iterate through every button to disable it (temporary)
+    for(button of buttons) {
+        button.disabled = true;
+    }
+}
 
 // event listener on play, to pause automatically after 3 seconds
 media.addEventListener('play', (e) => {
     
-    // timeout after 3 seconds
-        // media.pause();
-        // enable all buttons
+    // wait 3 seconds
+    setTimeout(function() {
+        // pause video
+        media.pause();
 
+        // iterate through every button and enable buttons again
+        for(button of buttons) {
+            button.disabled = false;
+        }
+    }, 3000);
 });
